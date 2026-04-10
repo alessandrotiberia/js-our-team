7c7d076f1d4705a953ce36c4baa9414ac4c5c0eb
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-10 16:41:27 +0200

    ora le immagini sono visualizzate correttamente

--- assets/scripts/teams.js
+++ assets/scripts/teams.js
@@ -3,42 +3,42 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "./assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "./assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "./assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "./assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "./assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "./assets/img/female3.png"
   }
 ];
 
 /*Dato un array di oggetti rappresentante un team di un’azienda,
-creare una pagina dedicata  in cui mostrare una card per ciascun componente. */
+creare una pagina dedicata in cui mostrare una card per ciascun componente. */
 
 const teamContainer = document.getElementById("team-container");
 
@@ -46,7 +46,7 @@ const teamContainer = document.getElementById("team-container");
 for (let i = 0; i < teamMembers.length; i++) {
   const member = teamMembers[i];
 
-// Creiamo la struttura HTML della card usando i template literal (gli apici inversi ` )
+// Creiamo la struttura HTML della card usando i template literals (gli apici inversi ` )
   const cardHTML = `
     <div class="card">
       <img src="${member.img}" alt="Foto di ${member.name}">
